abee58181b558383313609365ab24ece5cd0d0a5
Asia <joan.mk7@gmail.com>
2023-01-12 20:57:52 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EducationPageComponent } from './education-page/education-page.component';
+import { MyPageComponent } from './my-page/my-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { SkillsPageComponent } from './skills-page/skills-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MyPageComponent, },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'education', component: EducationPageComponent },
+  { path: 'skills', component: SkillsPageComponent },
+  { path: 'projects', component: ProjectsPageComponent }
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
