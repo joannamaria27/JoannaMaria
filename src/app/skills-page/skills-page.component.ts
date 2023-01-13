12bd6bc729d1a3a4739d69d4593416a81158eb84
Asia <joan.mk7@gmail.com>
2023-01-13 16:45:23 +0100

    add list skills

--- src/app/skills-page/skills-page.component.ts
+++ src/app/skills-page/skills-page.component.ts
@@ -1,25 +1,50 @@
+import { Binary } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faAngular, faCss3, faHtml5, faJs, faFigma, faNode, faBootstrap } from '@fortawesome/free-brands-svg-icons';
-import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { IconPrefix } from '@fortawesome/fontawesome-svg-core';
+import { faAngular, faCss3Alt, faHtml5, faJs, faFigma, faGitAlt, faBootstrap, IconName } from '@fortawesome/free-brands-svg-icons';
+import { faCode, faDatabase, faLanguage, faLaptopCode, faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
 
+const SkillsList = [
+  { name: 'Angular', sup: ' ', rate: 4.0, icon: 'angular' as IconName, type: 'fab' as IconPrefix },
+  { name: 'JavaScript', sup: '+ VSC', rate: 4.0, icon: 'js' as IconName, type: 'fab' as IconPrefix },
+  { name: 'TypeScript', sup: ' ', rate: 4.0, icon: 'code' as IconName, type: 'fas' as IconPrefix },
+  { name: 'Git', sup: '+ Github', rate: 4.0, icon: 'git-alt' as IconName, type: 'fab' as IconPrefix },
+  { name: 'HTML5', sup: '+ Bootstrap ', rate: 5.0, icon: 'html5' as IconName, type: 'fab' as IconPrefix },
+  { name: 'CSS3', sup: ' ', rate: 4.0, icon: 'css3-alt' as IconName, type: 'fab' as IconPrefix },
+  { name: 'UX, UI', sup: '+ Adobe XD, Figma', rate: 4.0, icon: 'figma' as IconName, type: 'fab' as IconPrefix },
+  { name: 'SQL, RDBMS', sup: '+ Oracle, Microsoft SQL Server', rate: 4.0, icon: 'database' as IconName, type: 'fas' as IconPrefix },
+  { name: 'C#, .NET', sup: '+ VS', rate: 4.0, icon: 'laptop-code' as IconName, type: 'fas' as IconPrefix },
+  { name: 'Teamwork', sup: ' ', rate: 5.0, icon: 'people-group' as IconName, type: 'fas' as IconPrefix },
+
+];
 
 @Component({
   selector: 'app-skills-page',
   templateUrl: './skills-page.component.html',
   styleUrls: ['./skills-page.component.css']
 })
+
+
+
 export class SkillsPageComponent {
-  currentRate = 3.14;
   constructor(library: FaIconLibrary) {
     library.addIcons(
       faAngular,
-      faCss3,
+      faCss3Alt,
       faHtml5,
       faJs,
-      faLanguage
-
-
+      faLanguage,
+      faFigma,
+      faGitAlt,
+      faDatabase,
+      faPeopleGroup,
+      faCode,
+      faLaptopCode
     );
   }
+
+
+  skillsList = SkillsList;
+
 }
